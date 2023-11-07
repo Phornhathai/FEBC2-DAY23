0aec28b423d9030d42e24678a7581c52a7e8f397
Phornhathai <phornhathai.aum@gmail.com>
2023-11-07 19:19:20 +0700

    Update Day23

---
+++ React-project/src/components/WrappedApp.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { HashRouter } from 'react-router-dom'
+import App from '../App'
+
+
+function WrappedApp() {
+  return (
+   <HashRouter>
+    <App/>
+   </HashRouter>
+  )
+}
+
+export default WrappedApp
